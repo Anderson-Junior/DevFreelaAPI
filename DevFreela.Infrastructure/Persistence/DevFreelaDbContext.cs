64e69f6964cd3076c94f26b23d61b26c3048e529
Anderson da Silva Lemos Junior <anderson.lemos@tivit.com>
2021-10-19 13:47:29 -0300

    Refatorando as Configurações

--- DevFreela.Infrastructure/Persistence/DevFreelaDbContext.cs
+++ DevFreela.Infrastructure/Persistence/DevFreelaDbContext.cs
@@ -1,37 +1,24 @@
 ﻿using DevFreela.Core.Entities;
-using System;
-using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
 
 namespace DevFreela.Infrastructure.Persistence
 {
-    public class DevFreelaDbContext
+    public class DevFreelaDbContext : DbContext
     {
-        public List<Project> Projects { get; set; }
-        public List<User> Users { get; set; }
-        public List<Skill> Skills { get; set; }
-        public List<ProjectComment> ProjectComments { get; set; }
-        public DevFreelaDbContext()
+        public DevFreelaDbContext(DbContextOptions<DevFreelaDbContext> options) : base(options)
         {
-            Projects = new List<Project>
-            {
-                new Project("Meu projeto ASP.NET CORE 1", "Descrição", 1, 1, 10000),
-                new Project("Meu projeto ASP.NET CORE 2", "Descrição", 1, 1, 10000),
-                new Project("Meu projeto ASP.NET CORE 3", "Descrição", 1, 1, 10000)
-            };
+          
+        }
+        public DbSet<Project> Projects { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<UserSkill> UserSkills { get; set; }
+        public DbSet<Skill> Skills { get; set; }
+        public DbSet<ProjectComment> ProjectComments { get; set; }
 
-            Users = new List<User>
-            {
-                new User("Anderson", "anderson@gmail.com", new DateTime(2001, 6, 2)),
-                new User("Junior", "junior@gmail.com", new DateTime(2000, 2, 3)),
-                new User("Pedro", "pedro@gmail.com", new DateTime(2002, 8, 9))
-            };
-
-            Skills = new List<Skill>
-            {
-                new Skill("C#"),
-                new Skill("ASP.NET CORE"),
-                new Skill("SQLSERVER"),
-            };
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
         }
     }
 }
